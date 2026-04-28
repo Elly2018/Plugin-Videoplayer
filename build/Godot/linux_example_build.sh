7ad8d9eb49077117e488ff7abd05960afb14fc8c
Zhu_Elly <chu.elly@funique.tv>
2026-04-28 15:11:21 +0800

    Added cmake build script

--- build/Godot/linux_example_build.sh
+++ build/Godot/linux_example_build.sh
@@ -2,4 +2,4 @@
 projectpath=example/Godot/addons/videoplayer/lib/Linux-AMD64
 
 cd ../..
-cp -f "GDExtensionTemplate-build\GDExtensionTemplate\lib\Linux-AMD64\Debug\gd_videoplayer-d.dll" $projectpath
+cp -f "build_godot_linux_amd64\lib\Linux-AMD64\Debug\gd_videoplayer-d.dll" $projectpath
