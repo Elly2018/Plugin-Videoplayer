d6aaee7b4cffb6d2234b5a3af8b566a5b7a00969
Zhu_Elly <chu.elly@funique.tv>
2026-04-28 13:53:49 +0800

    Change cmake version and c++ version

--- build/Godot/linux_codegen.sh
+++ build/Godot/linux_codegen.sh
@@ -3,4 +3,4 @@ cd ../..
 cd src
 echo Linux>platform.txt
 cd ..
-cmake -B GDExtensionTemplate-build -G"Visual Studio 17 2022" -DEngine=Godot -DPlatfrom=Linux -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=GDExtensionTemplate-install elly_videoplayer
+cmake -B GDExtensionTemplate-build -DEngine=Godot -DPlatfrom=Linux -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=GDExtensionTemplate-install elly_videoplayer
