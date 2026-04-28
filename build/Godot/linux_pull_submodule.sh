1a05b586d52109f9ebb3aae5d9439ce3d9508a17
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-04-28 21:35:35 +0800

    Rename folder fixed

--- build/Godot/linux_pull_submodule.sh
+++ build/Godot/linux_pull_submodule.sh
@@ -4,5 +4,5 @@ git clone -b 4.1 https://github.com/godotengine/godot-cpp.git
 cd src
 curl --output ffmpeg.tar.xz --ssl-no-revoke -L -O https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2026-02-28-12-59/ffmpeg-N-123074-g4e32fb4c2a-linux64-gpl-shared.tar.xz
 tar -xf ffmpeg.tar.xz
-mv ffmpeg-master-latest-linux64-gpl-shared ffmpeg
+mv ffmpeg-N-123074-g4e32fb4c2a-linux64-gpl-shared ffmpeg
 rm ./ffmpeg.tar.xz
