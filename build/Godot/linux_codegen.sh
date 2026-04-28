64652f2e08fe8a14550367326efb89e689ec2551
Zhu_Elly <chu.elly@funique.tv>
2026-04-28 14:02:29 +0800

    Remove dll build output

--- build/Godot/linux_codegen.sh
+++ build/Godot/linux_codegen.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
-cd ../..
-cd src
+#!/bin/bash
+cd ../../src
+rm -f platform.txt
 echo Linux>platform.txt
 cd ..
-cmake -B GDExtensionTemplate-build -DEngine=Godot -DPlatfrom=Linux -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=GDExtensionTemplate-install elly_videoplayer
+mkdir build_godot_linux_amd64
+cd build_godot_linux_amd64
+cmake .. -B GDExtensionTemplate-build -DEngine=Godot -DPlatfrom=Linux -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=GDExtensionTemplate-install elly_videoplayer
