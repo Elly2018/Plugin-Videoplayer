882bba1b1cdc5b29cbec0709a03618ef63b82039
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-05-03 01:18:07 +0800

    Update window build script, targeting visual studio 17 2022, prevent cross platform build issue

--- build/Godot/windows_codegen.bat
+++ build/Godot/windows_codegen.bat
@@ -2,4 +2,4 @@
 cd ../../src
 echo Windows>platform.txt
 cd ..
-cmake -B build_godot_window_x64 -DEngine=Godot -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=GDExtensionTemplate-install
+cmake -G "Visual Studio 17 2022" -B build_godot_window_x64 -DEngine=Godot -DPlatfrom=Windows -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=GDExtensionTemplate-install
